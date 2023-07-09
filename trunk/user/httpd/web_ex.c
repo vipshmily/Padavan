941fe9a60cd822fc17be7b66adad3c2883be4d9a
vipshmily <85991289+vipshmily@users.noreply.github.com>
2023-07-09 19:05:20 +0800

    add freememory btn on top banner

--- trunk/user/httpd/web_ex.c
+++ trunk/user/httpd/web_ex.c
@@ -3584,6 +3584,13 @@ apply_cgi(const char *url, webs_t wp)
 		websRedirect(wp, current_url);
 		return 0;
 	}
+	else if (!strcmp(value, " FreeMemory "))
+	{
+		doSystem("sync");
+		doSystem("echo 3 > /proc/sys/vm/drop_caches");
+		websRedirect(wp, current_url);
+		return 0;
+	}
 	else if (!strcmp(value, " RestoreNVRAM "))
 	{
 		websApply(wp, "Restarting.asp");
